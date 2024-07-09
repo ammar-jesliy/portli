4cec7ab11aa83a49135a258d8c5419c6af5b5d94
ammar-jesliy <ammarjc1@gmail.com>
2024-07-09 18:10:32 +0530

    Add checkmark to active theme

--- app/admin/_components/Themes.jsx
+++ app/admin/_components/Themes.jsx
@@ -3,39 +3,45 @@
 import { useContext } from "react";
 import themes from "../../../app/_data/themeData";
 import { AdminContext } from "../../_context/AdminContext";
+import { BadgeCheck } from "lucide-react";
 
 const Themes = () => {
 
-  const { setTheme } = useContext(AdminContext);
+  const { setTheme, theme } = useContext(AdminContext);
 
   return (
     <div className="absolute grid grid-cols-2 lg:grid-cols-3 px-2 sm:px-4 py-3 w-full max-h-80 overflow-y-scroll bg-base-100 left-0 top-[-10px] translate-y-[-100%] rounded-2xl border-2 border-base-300 animate-slide-up z-[-1] scrollbar-hidden">
-      {themes.map((theme, index) => (
+      {themes.map((themeData, index) => (
         <button 
           key={index} 
-          className="btn rounded-md px-[1px] m-1 text-xs lg:text-sm " 
-          data-theme={theme.name}
-          onClick={() => setTheme(theme.name)}
+          className="btn rounded-md px-[1px] m-1 text-xs lg:text-sm relative overflow-hidden" 
+          data-theme={themeData.name}
+          onClick={() => setTheme(themeData.name)}
         >
+          {theme === themeData.name && 
+            <div className="absolute right-0 w-1/3 flex items-center justify-center bg-green-400 h-full rounded-l-full ">
+              <BadgeCheck size={24} color="white" />
+            </div>
+          }
           <div className="flex gap-[2px]">
             <div
               className="w-2 h-6 rounded-full"
-              style={{ backgroundColor: theme.primary }}
+              style={{ backgroundColor: themeData.primary }}
             ></div>
             <div
               className="w-2 h-6 rounded-full"
-              style={{ backgroundColor: theme.secondary }}
+              style={{ backgroundColor: themeData.secondary }}
             ></div>
             <div
               className="w-2 h-6 rounded-full"
-              style={{ backgroundColor: theme.accent }}
+              style={{ backgroundColor: themeData.accent }}
             ></div>
             <div
               className="w-2 h-6 rounded-full"
-              style={{ backgroundColor: theme["base-100"] }}
+              style={{ backgroundColor: themeData["base-100"] }}
             ></div>
           </div>
-          <p className="w-[55px] text-left" >{theme.name}</p>
+          <p className="w-[55px] text-left" >{themeData.name}</p>
         </button>
       ))}
     </div>
