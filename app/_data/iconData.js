729c26afb64afc4893c4e9eb675d6d8b8c7108a5
ammar-jesliy <ammarjc1@gmail.com>
2024-07-16 17:29:21 +0530

    Add socialIcons data file to map

---
+++ app/_data/iconData.js
@@ -0,0 +1,55 @@
+import {
+  SiYoutube,
+  SiInstagram,
+  SiGithub,
+  SiLinkedin,
+  SiFacebook,
+  SiDribbble,
+  SiMedium,
+  SiX,
+  SiSnapchat,
+  SiTiktok,
+} from "@icons-pack/react-simple-icons";
+
+export default {
+  instagram: {
+    icon: SiInstagram,
+    color: `linear-gradient( to top right, #833ab4,#fd1d1d,#fcb045)`,
+  },
+  youtube: {
+    icon: SiYoutube,
+    color: "#cd201f",
+  },
+  x: {
+    icon: SiX,
+    color: "#000000",
+  }, // Note: Twitter is now X
+  facebook: {
+    icon: SiFacebook,
+    color: "#4267b2",
+  },
+  tiktok: {
+    icon: SiTiktok,
+    color: "#000000",
+  },
+  linkedin: {
+    icon: SiLinkedin,
+    color: "#0a66c2",
+  },
+  github: {
+    icon: SiGithub,
+    color: "#000000",
+  },
+  dribbble: {
+    icon: SiDribbble,
+    color: "#ea4c89",
+  },
+  medium: {
+    icon: SiMedium,
+    color: "#000000",
+  },
+  snapchat: {
+    icon: SiSnapchat,
+    color: "#fffc00",
+  },
+};
--- app/admin/_components/Socials.jsx
+++ app/admin/_components/Socials.jsx
@@ -20,54 +20,14 @@ import { userInfo, userSocials } from "../../../utils/schema";
 import { eq } from "drizzle-orm";
 import { toast } from "react-toastify";
 import { AdminContext } from "../../_context/AdminContext";
+import iconData from "../../_data/iconData";
 
 const Socials = () => {
   const [socialLinks, setSocialLinks] = useState({});
   const { user } = useUser();
   const { userDetails } = useContext(AdminContext);
 
-  const iconMap = {
-    instagram: {
-      icon: SiInstagram,
-      color: `linear-gradient( to top right, #833ab4,#fd1d1d,#fcb045)`,
-    },
-    youtube: {
-      icon: SiYoutube,
-      color: "#cd201f",
-    },
-    x: {
-      icon: SiX,
-      color: "#000000",
-    }, // Note: Twitter is now X
-    facebook: {
-      icon: SiFacebook,
-      color: "#4267b2",
-    },
-    tiktok: {
-      icon: SiTiktok,
-      color: "#000000",
-    },
-    linkedin: {
-      icon: SiLinkedin,
-      color: "#0a66c2",
-    },
-    github: {
-      icon: SiGithub,
-      color: "#000000",
-    },
-    dribbble: {
-      icon: SiDribbble,
-      color: "#ea4c89",
-    },
-    medium: {
-      icon: SiMedium,
-      color: "#000000",
-    },
-    snapchat: {
-      icon: SiSnapchat,
-      color: "#fffc00",
-    },
-  };
+  const iconMap = iconData;
 
   useEffect(() => {
     user && getExistingSocials();
