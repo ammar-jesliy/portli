495636307e7fad6548a00ea3adc06ed636f789de
ammar-jesliy <ammarjc1@gmail.com>
2024-07-22 16:30:04 +0530

    Add landing page content component

---
+++ app/_components/LandingContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const LandingContent = () => {
+  return (
+    <div className=''>
+        <Header />
+    </div>
+  )
+}
+
+export default LandingContent
--- app/page.js
+++ app/page.js
@@ -1,17 +1,8 @@
 import Link from "next/link";
+import LandingContent from "./_components/LandingContent";
 
 export default function Home() {
   return (
-    <div>
-      <header className="flex justify-between h-14 items-center px-10">
-        <div>
-          Logo
-        </div>
-        <div className="flex gap-4">
-          <Link href="/sign-in">Sign-in</Link>
-          <Link href="/sign-up">Sign-up</Link>
-        </div>
-      </header>
-    </div>
+    <LandingContent />
   );
 }
