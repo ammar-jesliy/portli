4b79747feeb5c5886c6a7b3619a0f8213f1e7a92
ammar-jesliy <ammarjc1@gmail.com>
2024-07-31 12:41:31 +0530

    Fix logo

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Logo from "../../public/Logo-text.svg"
 
 const authLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen w-screen">
       <div className="flex lg:flex-[5] flex-col w-full">
         <div className="w-full flex justify-center pb-8 pt-6 lg:pl-12 lg:justify-start">
-          <Image src="/logo-text.svg" width={100} height={45} alt="Portli"/>
+          <Image src={Logo} width={100} height={45} alt="Portli"/>
         </div>
         <div className="flex flex-1 self-center items-center pb-8 lg:px-12 lg:pb-12">
           {children}
