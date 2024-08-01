b9efee367108fc9e0954eb9e881433487132b9ac
ammar-jesliy <ammarjc1@gmail.com>
2024-08-01 13:58:06 +0530

    Add metadata to username route layout  and base layout

--- app/[username]/layout.jsx
+++ app/[username]/layout.jsx
@@ -1,11 +1,45 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../utils";
+import { userInfo } from "../../utils/schema";
 import UserPageProvider from "./Provider";
 
+async function fetchUserData(username) {
+  const result = await db
+    .select()
+    .from(userInfo)
+    .where(eq(userInfo.username, username));
+
+  if (result.length > 0) {
+    return {
+      name: result[0]?.name,
+      bio: result[0]?.bio,
+    };
+  }
+}
+
+export async function generateMetadata({ params }) {
+  const username = params.username;
+
+  const userData = await fetchUserData(username);
+
+  return {
+    title: (userData?.name || username) + " | Portli",
+    description: userData?.bio || "View the portli site of " + username,
+    openGraph: {
+      title: userData?.name || username,
+      description: userData?.bio || "View the portli site of " + username,
+      url: "https://portli.vercel.app/" + username,
+    },
+    twitter: {
+      card: "summary",
+      title: userData?.name || username,
+      description: userData?.bio || "View the portli site of " + username,
+    },
+  };
+}
+
 const layout = ({ children }) => {
-  return (
-    <UserPageProvider>
-      {children}
-    </UserPageProvider>
-  );
+  return <UserPageProvider>{children}</UserPageProvider>;
 };
 
 export default layout;
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Poppins, Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "leaflet/dist/leaflet.css";
 
 const poppins = Poppins({
@@ -22,15 +22,31 @@ const bricolage = Bricolage_Grotesque({
 export const metadata = {
   title: "Portli",
   description: "Micro site builder",
+  openGraph: {
+    title: "Portli",
+    description: "Micro site builder",
+    url: "https://portli.vercel.app",
+    images: [
+      {
+        url: "/portli-og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portli",
+    description: "Micro site builder",
+    images: ["/portli-og-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en" data-theme="light">
-        <body
-          className={`${poppins.variable} ${bricolage.variable} h-full`}
-        >
+        <body className={`${poppins.variable} ${bricolage.variable} h-full`}>
           {children}
           <ToastContainer />
         </body>
