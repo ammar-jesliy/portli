0d244a1a69b223ffd477e1efbaf677ab90bce7f7
ammar-jesliy <ammarjc1@gmail.com>
2025-01-13 10:57:59 +0530

    Update database URL to use environment variable

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: "postgresql://Portli_owner:ais2vqlfC1BV@ep-ancient-block-a1cl32er.ap-southeast-1.aws.neon.tech/Portli?sslmode=require",
+      url: process.env.NEXT_PUBLIC_DB_URL,
     }
   };
